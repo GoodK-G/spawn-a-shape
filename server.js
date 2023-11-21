f7be11ffb3acb0b3bb7b0aead71c2969ce80a453
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2023-11-21 17:52:44 +0000

    Reconfigured the questions and prompt to take advantage of the package that I used.

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const prompt = require('prompt');
+const prompts = require('prompts');
 const fs = require('fs');
 const createSvg = require('svg-parser');
-
-async function getUserInput()  {
-    const response =await prompts([
-        {
-            type: 'text',
-            name: 'text',
-            message: 'enter up to three characters for text inside of the shape: '
-        },
-        {
-            type: 'text',
-            name: 'textColor',
-            message: 'Enter the color of the text here (keyword or hexidecimal): '
-        },
-        {
-            type: 'select',
-            name: 'shape',
-            message:'choose a shape: ',
-            choices: [
-                {title: 'Circle', value:'circle'},
-                {title: 'Triangle', value:'triangle'},
-                {title: 'Square', value: 'square'}
-            ]
-        },
-        {
-            type: 'text',
-            name: 'shapeColor',
-            message: 'Enter shape color! (keyword or hexadecimal): '
-        }
-    ]);
-    return response;
-}
+//These are the questions for the different parameters that you will enter in to the console.
+const questions = [
+    {
+        type: 'text',
+        name: 'text',
+        message: 'enter up to three characters for text inside of the shape: '
+    },
+    {
+        type: 'text',
+        name: 'textColor',
+        message: 'Enter the color of the text here (keyword or hexidecimal): '
+    },
+    {
+        type: 'select',
+        name: 'shape',
+        message:'choose a shape: ',
+        choices: [
+            {title: 'Circle', value:'circle'},
+            {title: 'Triangle', value:'triangle'},
+            {title: 'Square', value: 'square'}
+        ]
+    },
+    {
+        type: 'text',
+        name: 'shapeColor',
+        message: 'Enter shape color! (keyword or hexadecimal): '
+    }
+];
+//this is the waiting for the server to run, and the prompts to be answered and this will be stored in the response const.
+(async () => {
+    const response = await prompts(questions);
+  })();
